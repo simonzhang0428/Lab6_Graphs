365f0665a92e655b12b9730592b7db8f6147e1c6
simon <simonzhang0428@gmail.com>
2020-06-07 12:01:19 -0700

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,7 +1,8 @@
 /**
  * Graph.java
- * @author
- * @author
+ *
+ * @author Daniil Durnev
+ * @author Simon Zhang
  * CIS 22C, Lab 6
  */
 
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 public class Graph {
     private int vertices;
     private int edges;
-    private ArrayList<List<Integer> > adj;
+    private ArrayList<List<Integer>> adj;
     private ArrayList<Character> color;
     private ArrayList<Integer> distance;
     private ArrayList<Integer> parent;
@@ -19,11 +20,25 @@ public class Graph {
 
     /**
      * initializes an empty graph, with n vertices
-     * and 0 edges, and intitializes all arraylists
+     * and 0 edges, and initializes all arraylists
      * to contain default values from indices 1 to n
+     *
      * @param n the number of vertices in the graph
      */
     public Graph(int n) {
+        vertices = n;
+        edges = 0;
+        adj = new ArrayList<>(n + 1);
+        color = new ArrayList<>(n + 1);
+        distance = new ArrayList<>(n + 1);
+        parent = new ArrayList<>(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            color.add(i, 'W');
+            distance.add(i, -1);
+            parent.add(i, 0);
+            adj.add(i, new List<>());
+        }
 
     }
 
@@ -32,74 +47,97 @@ public class Graph {
 
     /**
      * Returns the number of edges in the graph
+     *
      * @return the number of edges
      */
     public int getNumEdges() {
-        return -1;
+        return edges;
     }
 
     /**
      * Returns the number of vertices in the graph
+     *
      * @return the number of vertices
      */
     public int getNumVertices() {
-        return -1;
+        return vertices;
     }
 
     /**
      * returns whether the graph is empty (no vertices)
+     *
      * @return whether the graph is empty
      */
     public boolean isEmpty() {
-        return false;
+        return vertices == 0;
     }
 
     /**
      * Returns the value of the distance[v]
+     *
      * @param v a vertex in the graph
-     * @precondition 0 < v <= vertices
      * @return the distance of vertex v
      * @throws IndexOutOfBoundsException when
-     * the precondition is violated
+     *                                   the precondition is violated
+     * @precondition 0 < v <= vertices
      */
-    public Integer getDistance(Integer v) throws IndexOutOfBoundsException{
-        return -1;
+    public Integer getDistance(Integer v) throws IndexOutOfBoundsException {
+        if (v <= 0 || v > vertices) {
+            throw new IndexOutOfBoundsException("getDistance, out of bound.");
+        } else {
+            return distance.get(v);
+        }
     }
 
     /**
      * Returns the value of the parent[v]
+     *
      * @param v a vertex in the graph
-     * @precondition 0 < v <= vertices
      * @return the parent of vertex v
      * @throws IndexOutOfBoundsException when
-     * the precondition is violated
+     *                                   the precondition is violated
+     * @precondition 0 < v <= vertices
      */
     public Integer getParent(Integer v) throws IndexOutOfBoundsException {
-        return -1;
+        if (v <= 0 || v > vertices) {
+            throw new IndexOutOfBoundsException("getParent, out of bound.");
+        } else {
+            return parent.get(v);
+        }
     }
 
     /**
      * Returns the value of the color[v]
+     *
      * @param v a vertex in the graph
-     * @precondition 0 < v <= vertices
      * @return the color of vertex v
      * @throws IndexOutOfBoundsException when
-     * the precondition is violated
+     *                                   the precondition is violated
+     * @precondition 0 < v <= vertices
      */
     public Character getColor(Integer v) throws IndexOutOfBoundsException {
-        return null;
+        if (v <= 0 || v > vertices) {
+            throw new IndexOutOfBoundsException("getColor, out of bound.");
+        } else {
+            return color.get(v);
+        }
     }
 
     /**
      * Returns the List stored at index v
+     *
      * @param v a vertex in the graph
      * @return the adjacency List a v
+     * @throws IndexOutOfBoundsException when
+     *                                   the precondition is violated
      * @precondition 0 < v <= vertices
-     * @throws IndexOutOfBoundsException  when
-     * the precondition is violated
      */
     public List<Integer> getAdjacencyList(Integer v) throws IndexOutOfBoundsException {
-        return null;
+        if (v <= 0 || v > vertices) {
+            throw new IndexOutOfBoundsException("getAdjacencyList, out of bound.");
+        } else {
+            return adj.get(v);
+        }
     }
 
     /*** Manipulation Procedures ***/
@@ -107,24 +145,34 @@ public class Graph {
     /**
      * Inserts vertex v into the adjacency list of vertex u
      * (i.e. inserts v into the list at index u)
+     *
+     * @throws IndexOutOfBoundsException  when the precondition
+     *                          is violated
      * @precondition 0 < u <= vertices, 0 < v <= vertices
-     * @throws IndexOutOfBounds exception when the precondition
-     * is violated
      */
     public void addDirectedEdge(Integer u, Integer v) throws IndexOutOfBoundsException {
-
+        if ((u <= 0 || u > vertices) || (v <= 0 || v > vertices)) {
+            throw new IndexOutOfBoundsException("addDirectedEdge, out of bound.");
+        } else {
+            adj.get(u).addLast(v);
+        }
     }
 
     /**
      * Inserts vertex v into the adjacency list of vertex u
      * (i.e. inserts v into the list at index u)
      * and inserts u into the adjacent vertex list of v
+     *
+     * @throws IndexOutOfBoundsException  when the precondition
+     *                          is violated
      * @precondition 0 < u <= vertices, 0 < v <= vertices
-     * @throws IndexOutOfBounds exception when the precondition
-     * is violated
      */
     public void addUndirectedEdge(Integer u, Integer v) throws IndexOutOfBoundsException {
-
+        if ((u <= 0 || u > vertices) || (v <= 0 || v > vertices)) {
+            throw new IndexOutOfBoundsException("addUndirectedEdge, out of bound.");
+        } else {
+            adj.get(u).addLast(v);
+        }
     }
 
     /*** Additional Operations ***/
@@ -134,11 +182,14 @@ public class Graph {
      * Prints the adjacency list of each vertex in the graph,
      * vertex: <space separated list of adjacent vertices>
      */
-    @Override public String toString() {
+    @Override
+    public String toString() {
         String result = "";
-        return "";
+        for (int i = 1; i <= vertices; i++) {
+            result += Integer.toString(i) + adj.get(i);
+        }
+        return result;
     }
-
 
 
     /**
@@ -149,22 +200,50 @@ public class Graph {
      * Note that this method is intended purely to help you debug your code
      */
     public void printBFS() {
-
+        System.out.println("v\tc\tp\td\t");
+        for (int i = 1; i <= vertices; i++) {
+            System.out.println(Integer.toString(i) + "\t" +
+                    Character.toString(color.get(i)) + "\t" +
+                    Integer.toString(parent.get(i)) + "\t" +
+                    Integer.toString(distance.get(i)) + "\t");
+        }
     }
 
     /**
      * Performs breath first search on this Graph give a source vertex
+     *
      * @param source the source vertex
+     * @throws IllegalStateException     if the graph is empty
+     * @throws IndexOutOfBoundsException when vertex is
+     *                                   outside the bounds of the graph
      * @precondition graph must not be empty
      * @precondition source is a vertex in the graph
-     * @throws IllegalStateException if the graph is empty
-     * @throws IndexOutOfBoundsException when vertex is
-     * outside the bounds of the graph
      */
 
     public void BFS(Integer source) throws IllegalStateException, IndexOutOfBoundsException {
-
+        if (this.isEmpty()) {
+            throw new IllegalStateException("BFS, graph is empty.");
+        } else if (source <= 0 || source > vertices) {
+            throw new IndexOutOfBoundsException("BFS, index out of bound.");
+        } else {
+            color.set(source, 'G');
+            distance.set(source, 0);
+            List<Integer> temp = new List<>();
+            temp.addLast(source);
+            while (!temp.isEmpty()) {
+                int x = temp.getFirst();
+                temp.removeFirst();
+                for (int y = 1; y <= adj.get(x).getLength(); y++) {
+                    if (color.get(y) == 'W') {
+                        color.set(y, 'G');
+                        distance.set(y, distance.get(x)+1);
+                        parent.set(y, parent.get(x));
+                        temp.removeFirst();
+                    }
+                }
+                color.set(x, 'B');
+            }
+        }
     }
 
 }
-
